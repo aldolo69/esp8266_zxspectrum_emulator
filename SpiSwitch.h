ae8b0bb14a0ca95bdf0f0323c8c9a4398d9affe0
aldolo <aldolo69@users.noreply.github.com>
2020-01-22 10:19:38 +0100

    save to .z80

---
+++ SpiSwitch.h
@@ -0,0 +1,8 @@
+#ifndef SPISWITCH_H
+#define SPISWITCH_H 
+
+void spiSwitchSetup(void);
+void spiSwitchSet(int cs);
+void spiSwitchReset(void);
+
+#endif
---
+++ Zxsound.cpp
@@ -0,0 +1,13 @@
+#include "zxsound.h"
+ 
+extern unsigned char soundenabled;
+void  zxSoundSetup()
+{
+  pinMode(SPEAKER_CS, OUTPUT);
+}
+
+void  zxSoundSet(unsigned char c)
+{
+  digitalWrite(SPEAKER_CS, c&soundenabled);
+//DEBUG_PRINT(c);
+}
